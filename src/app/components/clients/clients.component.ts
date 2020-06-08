c1821e50f2a8bf99f90eae8f09cac215bbea21cc
David <liwei5220@gmail.com>
2020-06-08 14:02:25 +1200

    update1

---
+++ src/app/components/clients/clients.component.ts
@@ -0,0 +1,26 @@
+import { Component, OnInit } from '@angular/core';
+import { Client } from 'src/app/models/client';
+import { ClientService } from 'src/app/services/client.service';
+
+@Component({
+  selector: 'app-clients',
+  templateUrl: './clients.component.html',
+  styleUrls: ['./clients.component.css']
+})
+export class ClientsComponent implements OnInit {
+  clients: Client[];
+  x: any;
+  constructor(private clientService: ClientService) { }
+
+  ngOnInit(): void {
+    // this.clientService.getX().forEach(element => {
+    //   console.log(element);
+    // });
+    
+      this.clientService.getClients().subscribe(client =>
+        console.log(client));
+      
+    
+  }
+
+}
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,17 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyCE6dQmiIJkDHa_cgAN3SsAryV_YPTlVhQ",
+    authDomain: "clientpanelprod-b787e.firebaseapp.com",
+    databaseURL: "https://clientpanelprod-b787e.firebaseio.com",
+    projectId: "clientpanelprod-b787e",
+    storageBucket: "clientpanelprod-b787e.appspot.com",
+    messagingSenderId: "616217208370",
+    appId: "1:616217208370:web:0378e2ee2f27ffa648be2b",
+    measurementId: "G-D1FQMEW0KQ"
+  }
 };
 
 /*
